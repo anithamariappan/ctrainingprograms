bc765624edbe015e47d7fc23d2797a7ead6a23cf
anithamariappan <anithaani56358@gmail.com>
2017-06-22 16:43:04 +0530

    Add files via upload

---
+++ EX2.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float a,b,c,d,e,t;
+clrscr();
+printf("Enter the value of a : ");
+scanf("%f",&a);
+printf("Enter the value of b : ");
+scanf("%f",&b);
+printf("Enter the value of c : ");
+scanf("%f\n",&c);
+printf("Enter the value of d :");
+scanf("%f",&d);
+e=a+b+c+d;
+printf("The Sum of Four Numbers is %f \n :",e);
+t=e/4;
+printf("Average of Four number is %f \n : ",t);
+getch();
+}
+
+
+
