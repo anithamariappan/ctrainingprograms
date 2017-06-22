0d139c091286608d730bb091c841252a59af46b8
anithamariappan <anithaani56358@gmail.com>
2017-06-22 16:41:55 +0530

    Add files via upload

---
+++ ASCII.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char c;
+clrscr();
+printf("Enter a Character \n");
+scanf("%s",&c);
+printf("The Entered Character is %c \n",c);
+printf("ASCII Character is %d",c);
+getch();
+}
+
+
+
